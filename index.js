25307a041cca5601492a000c387a697bebd717ae
Daniel Fořt <danielfort2005@gmail.com>
2024-09-19 16:40:56 +0000

    feat: add file metadata functionality

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
+
 var app = express();
 
 app.use(cors());
@@ -11,8 +14,21 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), ( req, res ) => {
+  if( !req.file ){
+    res.send("Could not find the file.");
+    return;
+  }
+  const file = req.file;
 
+  file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf8')
 
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size
+  })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
